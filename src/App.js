13a047fff6ada5a9f157d7c167595c39aa1f8708
VukasinV <vranic.vukasin@gmail.com>
2022-04-02 00:13:48 +0200

    added rect with basic setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import NavMenu from "./components/nav-menu/NavMenu";
+
+function App() {
+  return (
+    <div className="container">
+      <div>
+        <h1>Vukasin Vranic</h1>
+        <p>Software engineer</p>
+      </div>
+      <NavMenu />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/nav-menu/NavMenu.js
@@ -0,0 +1,12 @@
+import "./NavMenu.css";
+
+function NavMenu() {
+  return (
+    <div className="nav-menu">
+      <div className="nav-menu__element">Home</div>
+      <div className="nav-menu__element">Blog</div>
+    </div>
+  );
+}
+
+export default NavMenu;
